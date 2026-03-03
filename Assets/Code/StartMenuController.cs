51d8c66af01210efbc62c075ae835d7a8103ec45
Molly-Doyle <N00254095@iadt.ie>
2026-03-03 22:07:15 +0000

    Fixed UI menus
    FINALLY took me all day ngl

---
+++ Assets/Code/StartMenuController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+
+public class StartMenuController : MonoBehaviour
+{   
+    public void OnStartClick()
+    {
+        SceneManager.LoadScene("LevelOne");
+    }
+
+public void OnExitClick()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+
+        Application.Quit();
+    }
+
+}
