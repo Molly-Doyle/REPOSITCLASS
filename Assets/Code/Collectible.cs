7d7419fb454bc7e663f854b2c17616386fa7a2a9
Molly-Doyle <N00254095@iadt.ie>
2026-04-21 12:36:07 +0100

    Fixed Collectible / Adding quest system

--- Assets/Code/Collectible.cs
+++ Assets/Code/Collectible.cs
@@ -1,40 +1,26 @@
+using System;
 using TMPro;
 using UnityEngine;
 
 public class Collectible : MonoBehaviour
 {
-    [SerializeField] private float rotationSpeed = 90f;
-    [Header("Pickup Effect")]
-    [SerializeField] private ParticleSystem pickupEffectPrefab; // assign in inspector
+    [SerializeField] private float rotationSpeed = 90f; // degrees per second
 
-    void Update()
+    private int trashCounter = 0;
+    public TMP_Text counterText;
+    private void Update()
     {
         transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0f);
     }
-
-    private int trashCounter = 0;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-
-
-            // Spawn pickup particle effect
-            if (pickupEffectPrefab != null)
-            {
-                ParticleSystem effect = Instantiate(pickupEffectPrefab, transform.position, Quaternion.identity);
-                effect.Play();
-
-                // Optional: destroy the particle system after it finishes
-                Destroy(effect.gameObject, effect.main.duration + effect.main.startLifetime.constantMax);
-            }
-
-            // Destroy the collectible
+           trashCounter ++;
+           counterText.text = "Collected Trash: " + trashCounter;
+        
             Destroy(gameObject);
-
-            //Add to the counter
-            trashCounter +=1;
         }
     }
 }
---
+++ Assets/Code/Quest.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="Quest")]
+public class Quest : MonoBehaviour
+{
+    public string questID;
+    public string questName;
+    public string description;
+    public List<QuestObjectives> objectives;
+
+//called when scriptable obj is created 
+    private void OnEnable()
+    {
+        if(string.IsNullOrEmpty(questID))
+        {
+            questID = questName + Guid.NewGuid().ToString();
+            
+        }
+    }
+
+    [System.Serializable]
+    public class QuestObjectives
+    {
+        public string objectiveID; //Note to self match with the item ID  you need to collect
+        public string description;
+        public ObjectiveType type;
+        public int requiredAmount;
+        public int currentAmount;
+
+        public bool IsComlpeted => currentAmount >= requiredAmount;
+    }
+
+    public enum ObjectiveType { CollectItem, ReachLocation, TalkNPC}
+
+    [System.Serializable]
+    public class QuestProgress
+
+    {
+        public Quest quest;
+        public List<QuestObjectives> objectives;
+
+        public QuestProgress(Quest quest)
+        {
+        this.quest = quest;
+        objectives = new List<QuestObjectives>();
+
+        //Deep copy to avoid modifying the og
+        foreach(var obj in quest.objectives)
+            {
+                objectives.Add(new QuestObjectives
+                {
+                    objectiveID = obj.objectiveID,
+                    description = obj.description,
+                    type = obj.type,
+                    requiredAmount = obj.requiredAmount,
+                    currentAmount = 0
+                });
+            }
+        }
+        public bool IsComlpeted => objectives.TrueForAll(o => o.IsComlpeted);
+        public string QuestID => quest.questID;
+    }
+}
